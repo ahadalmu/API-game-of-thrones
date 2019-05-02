bbf7c779d56aa9fef4b8927968059e7a30c78dfe
Ahad alanazi <ahadamash@gmail.com>
2019-05-02 09:44:53 +0300

    GOT

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 $(document).ready(function () {
 
+    var favChar = [];
+
+    function renderChar() {
+
+        if(localStorage.getItem('favChar') !== null){
+          favChar = JSON.parse(localStorage.getItem('favChar'));
+          favChar.forEach(function(char){
+            $('<li>').text(char).appendTo($('#fav'));
+          })
+        }
+        
+      }
+     
+      renderChar();
+
+      $('#fav').hide();
+      $('#fav').click(function () {
+          $('span', this).toggle();
+      });
+      $('#fav').click(function () {
+          $('#fav').toggle('slow');
+      });
+
+
     var submit = $('form');
     submit.on('submit', function (event) {
         var takeName = $('#name').val();
         console.log(takeName)
         event.preventDefault();
+        $('body').css('background-image', "url(./img/poster2.jpg)");
 
 
         $.ajax({
@@ -12,31 +37,41 @@ $(document).ready(function () {
             url: 'https://anapioficeandfire.com/api/characters?name='+takeName,
             success: function (resulte) {
                 console.log(resulte);
+                console.log(resulte[0].name);
+
 
                 var $h2 = $('<h2/>').append('section');
-                $('#info').text("Every thing about");
-                $('#info').css({'color': 'pink', 'size': '38px'});
+                $('#name1').text("Every thing you want to know about: "+resulte[0].name);     
+                $('#name1').css({'color': 'pink', 'size': '38px'});
                 
-
-                // var $name = $('<p/>').append('section');
-                $('#name').text("Name:"+resulte[0].name);
 
              
-                // var $pCulture = ('section').append$('<p/>')
-                $('#culture').text("Culture: "+resulte[0].culture);
+                
+                $('#culture').html("<strong>Culture: </strong>"+resulte[0].culture);
 
-                // var $pGender = $('<p/>').append('section');
                 $('#gender').text("Gender: "+resulte[0].gender);
                 
-                // var $pBorn = $('<p/>').append('section');
+             
                 $('#born').text("Born: "+resulte[0].born);
-                
-                // var $pTitles = $('<p/>').append('section');
+           
                 $('#titles').text("Titles: "+resulte[0].titles[0]);
 
                 $('#playedBy').text("Played By: "+resulte[0].playedBy[0]);
-                // $('#titles').text("Titles: "+resulte[0].titles[0]);
-                // $('#titles').text("Titles: "+resulte[0].titles[0]);
+             
+            
+                  
+                  $('<button/>').appendTo('section').text('like').on('click', function(event){
+
+                    event.preventDefault();
+              
+                    // var value = $('#listInput').val();
+                    // $()
+                    favChar.push(takeName);
+                    localStorage.setItem('favChar', JSON.stringify(favChar));
+                    $('<li>').text(takeName).appendTo($('#fav')).css({'list-style':'none','font-family':'Just Another Hand', 'color':'pink'});
+                  
+                 
+                })
 
             },
             error: function (error) {
@@ -45,6 +80,7 @@ $(document).ready(function () {
             }
         })
 
+        
     });
 
 
@@ -55,9 +91,9 @@ $(document).ready(function () {
             console.log(resulte);
 
             $('#quote').text(resulte.quote);
+            $('#quote').css('color','HONEYDEW');
             $('#character').text("By: "+resulte.character);
            
-
 
         },
         error: function (error) {
@@ -65,4 +101,19 @@ $(document).ready(function () {
 
         }
     })
+    
+    // $("#fav").hide();
+
+    // $("#fav1").click(function(){
+    //   $this.parent().next("#fav").toggle(slow);
+    //   return false;
+    // });
+
+    // $('#logo').on('click', togglelogo);
+
+    // function togglelogo(){
+    //     console.log('hiii')
+    //     $('#got-info').toggleClass('show');
+    //   }
+    
 })
